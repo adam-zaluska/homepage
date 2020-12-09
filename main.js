00064ec6f0ebb5e441e18fc4584ebace2e11b6cd
Adam Załuska <adamzaluska@10g.pl>
2020-12-09 11:43:18 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+alert ('Siema!')
+
+
+
+const firstName = 'Adam';
+const age = 32;
+
+console.log('Siema')
+console.log(firstName);
+console.log(age);
+console.log(`Witaj drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`)
+
